0f1a6464b533b8e24cb4817c926dfc63099028e9
Zoinker <MrZoinker@gmail.com>
2015-11-28 15:50:52 +0000

    Check my Skillz!

--- public/scripts/charGen.js
+++ public/scripts/charGen.js
@@ -11,17 +11,25 @@ var Generator = (function() {
 	var varInt;
 	var varCha;
 	
-	var temp = [5,5,5,5,5];
+	var attTemp = [5,5,5,5,5];
+	var skillTemp = [];
+	for (var i = 0; i < 16; i++){
+		skillTemp[i] = false;
+	}
+	
 	
 	var pointsLeft;
 	var attrib;
 	var total = 30;
 	var nym;
+	var skills;
+	var skillMax = 3;
 	
 	function init() {
 		form = document.getElementById('form');
 		pointsLeft = document.querySelector('.pointsLeft');
 		nym = document.querySelector('.name');
+		skills = document.querySelector('div.skills');
 		varStr = document.getElementById('str');
 		varAgi = document.getElementById('agi');
 		varCon = document.getElementById('con');
@@ -36,12 +44,12 @@ var Generator = (function() {
 		varCon.addEventListener('change', formCalc);
 		varInt.addEventListener('change', formCalc);
 		varCha.addEventListener('change', formCalc);
+		skills.addEventListener('change', skillCheck);
 	}
 	function formCalc() {
 		var values = {};
 		var inputs = form.getElementsByClassName('attrib');
 		var sum = 30;
-		console.log(":)");
 		
 		for (var i = 0; i < inputs.length; i++)
 		{
@@ -50,26 +58,45 @@ var Generator = (function() {
 		}
 		if (sum < 0){
 			for (var i = 0; i < inputs.length; i++){
-				inputs[i].value = temp[i];
+				inputs[i].value = attTemp[i];
 			}
 		}else{
 			for (var i = 0; i < inputs.length; i++){
-				temp[i] = inputs[i].value;
+				attTemp[i] = inputs[i].value;
 			}
 			var streng = "Points Left " + parseInt(sum);
 			pointsLeft.textContent = streng;
 		}
-		/*
-		var selects = form.getElementsByTagName('select')
-		for (var i = 0; i < selects.length; i++)
-		{
-			values[selects[i].name] = selects[i].value;
-		}
-		*/
     }
 	
-	function totalUpd1() {
-		console.log("Change!");
+	function skillCheck() {
+		console.log(":)");
+		var chkBox = skills.getElementsByClassName('skillz');
+		var checked = 0;
+		var upd = [];
+		var uPoint = 0;
+		for (var i = 0; i < chkBox.length; i++){
+			if(chkBox[i].checked === true){
+				checked++;
+			}
+			if(chkBox[i].checked !== skillTemp[i]){
+				upd[uPoint] = i;
+				uPoint++;
+			}
+		}
+		if(checked <= skillMax){
+			var a;
+			for (var i = 0; i < uPoint; i++){
+				a = upd[i];
+				skillTemp[a] = chkBox[a].checked;
+			}
+		}else{
+			var a;
+			for (var i = 0; i < uPoint; i++){
+				a = upd[i];
+				chkBox[a].checked = skillTemp[a];
+			}
+		}
 	}
 	
 	return {
