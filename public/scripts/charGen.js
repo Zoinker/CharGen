34fc3fce1965bae558f0e179eb6ff239dc4aecf6
simonzettler <sjz1@hi.is>
2015-11-30 15:08:01 +0000

    +putting imagesPreload.js and sprite.js to picture.js

--- public/scripts/charGen.js
+++ public/scripts/charGen.js
@@ -1,3 +1,5 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded',function() {
 	Generator.init();
 	console.log("Dom Loaded!");
@@ -10,14 +12,14 @@ var Generator = (function() {
 	var varCon;
 	var varInt;
 	var varCha;
-	
+
 	var attTemp = [5,5,5,5,5];
 	var skillTemp = [];
 	for (var i = 0; i < 16; i++){
 		skillTemp[i] = false;
 	}
-	
-	
+
+
 	var pointsLeft;
 	var attrib;
 	var description;
@@ -28,7 +30,7 @@ var Generator = (function() {
 	var skills;
 	var skillMax = 3;
 	var sChecked = 0;
-	
+
 	function init() {
 		form = document.getElementById('form');
 		pointsLeft = document.querySelector('.pointsLeft');
@@ -43,9 +45,9 @@ var Generator = (function() {
 		genButton = document.getElementById('generate');
 		formCalc();
 		skillCheck();
-		
+
 		//plugins.forEach(addPlugin);
-		
+
 		varStr.addEventListener('change', formCalc);
 		varAgi.addEventListener('change', formCalc);
 		varCon.addEventListener('change', formCalc);
@@ -55,28 +57,27 @@ var Generator = (function() {
 		genButton.addEventListener('click', generate);
 	}
 	function formCalc() {
-		var values = {};
+		//var values = {};
 		var inputs = form.getElementsByClassName('attrib');
 		pLeft = total;
-		
-		for (var i = 0; i < inputs.length; i++)
+
+		for (i = 0; i < inputs.length; i++)
 		{
 			var input = inputs[i];
 			pLeft -= input.value;
 		}
 		if (pLeft < 0){
-			for (var i = 0; i < inputs.length; i++){
+			for (i = 0; i < inputs.length; i++){
 				inputs[i].value = attTemp[i];
 			}
 		}else{
-			for (var i = 0; i < inputs.length; i++){
+			for (i = 0; i < inputs.length; i++){
 				attTemp[i] = inputs[i].value;
 			}
-			var streng = "Points Left " + parseInt(pLeft);
-			pointsLeft.textContent = streng;
+			pointsLeft.textContent = "Points Left " + parseInt(pLeft);
 		}
     }
-	
+
 	function skillCheck() {
 		console.log(":)");
 		var chkBox = skills.getElementsByClassName('skillz');
@@ -84,7 +85,7 @@ var Generator = (function() {
 		sChecked = 0;
 		var upd = [];
 		var uPoint = 0;
-		for (var i = 0; i < chkBox.length; i++){
+		for (i = 0; i < chkBox.length; i++){
 			if(chkBox[i].checked === true){
 				sChecked++;
 			}
@@ -95,21 +96,21 @@ var Generator = (function() {
 		}
 		if(sChecked <= skillMax){
 			var a;
-			for (var i = 0; i < uPoint; i++){
+			for (i = 0; i < uPoint; i++){
 				a = upd[i];
 				skillTemp[a] = chkBox[a].checked;
 			}
 		}else{
-			var a;
-			for (var i = 0; i < uPoint; i++){
+			//var a;
+			for (i = 0; i < uPoint; i++){
 				a = upd[i];
 				chkBox[a].checked = skillTemp[a];
 				sChecked = sCheckedOld;
 			}
 		}
 	}
-	
-	function generate(e) {
+
+	function generate() {
 		console.log("foo");
 
 		var charString = '';
@@ -121,13 +122,13 @@ var Generator = (function() {
 		var gName = document.getElementById('cname');
 		var inputs = form.getElementsByClassName('attrib');
 		var sum = 30;
-		
-		for (var i = 0; i < inputs.length; i++)
+
+		for (i = 0; i < inputs.length; i++)
 		{
 			var input = inputs[i];
 			sum -= input.value;
-		}	
-		
+		}
+
 		if(gName.value === ""){
 			valid = false;
 			charString = charString + "\n\nYou have no name. One can not adventure without a name!";
@@ -135,17 +136,17 @@ var Generator = (function() {
 			charString = "Names have power, but yours is far too powerful! Please choose a name that won't break the world!";
 			return;
 		}
-		
+
 		if(pLeft > 0){
 			valid = false;
 			charString = charString + "\n\nYou have yet to meet your full potential. Allocate all remaining attribute points before embarking on your adventure!";
 		}
-		
+
 		if(sChecked < skillMax){
 			valid = false;
 			charString = charString + "\n\nYou have not honed your skills. Pick three skills to aid you on your adventure!";
 		}
-		
+
 		if(valid === false){
 			charString = "You are not ready for your adventure:" + charString;
 		}else{
@@ -154,7 +155,7 @@ var Generator = (function() {
 			Save profile on backend if logged in
 			Generate description in textbox.
 			*/
-			
+
 			var nameString = gName.value;
 			var ageString = parseInt(gAge.value);
 			var raceString;
@@ -170,7 +171,7 @@ var Generator = (function() {
 					break;
 				default:
 					genderString = "";
-			}			
+			}
 			var genderString;
 			switch(gGender.options[gGender.value].text) {
 				case "Male":
@@ -182,59 +183,59 @@ var Generator = (function() {
 				default:
 					genderString = "person";
 			}
-			
+
 			var strString;
 			var strTemp = varStr.value;
 			if(strTemp < 3){strString = "You are a pathetic weakling. you can hardly lift a stick!"}else
 			if(strTemp < 5){strString = "You are pretty weak."}else
 			if(strTemp < 7){strString = "Your strength is average."}else
 			if(strTemp < 9){strString = "You are pretty strong."}else
-			{strString = "You are freakishly strong. You could lift a boulder with ease!"};
-			
+			{strString = "You are freakishly strong. You could lift a boulder with ease!"}
+
 			var agiString;
 			var agiTemp = varAgi.value;
 			if(agiTemp < 3){agiString = "You are as slow as a snail. You couldn't even escape a one legged troll!"}else
 			if(agiTemp < 5){agiString = "You are pretty slow."}else
 			if(agiTemp < 7){agiString = "Your agility is average."}else
 			if(agiTemp < 9){agiString = "You are pretty fast."}else
-			{agiString = "You are as fast as a rabbit. You can run circles around anybody!"};
-			
+			{agiString = "You are as fast as a rabbit. You can run circles around anybody!"}
+
 			var conString;
 			var conTemp = varCon.value;
 			if(conTemp < 3){conString = "You are as delicate as a reed. A swift wind could knock you dead!"}else
 			if(conTemp < 5){conString = "You are pretty delicate."}else
 			if(conTemp < 7){conString = "Your constitution is average."}else
 			if(conTemp < 9){conString = "You are pretty tough."}else
-			{conString = "You are as tough as a bull. Nothing can bring you down!"};
-			
+			{conString = "You are as tough as a bull. Nothing can bring you down!"}
+
 			var intString;
 			var intTemp = varInt.value;
 			if(intTemp < 3){intString = "You are a complete imbecile. It's a wonder you even remember how to breathe!"}else
 			if(intTemp < 5){intString = "You are pretty dumb."}else
 			if(intTemp < 7){intString = "Your intelligence is average."}else
 			if(intTemp < 9){intString = "You are pretty smart."}else
-			{intString = "You are a true genius. There is no problem you cannot solve with your superior intellect!"};
-			
+			{intString = "You are a true genius. There is no problem you cannot solve with your superior intellect!"}
+
 			var chaString;
 			var chaTemp = varCha.value;
 			if(chaTemp < 3){chaString = "You are a social outcast. No one wants anything to do with you!"}else
 			if(chaTemp < 5){chaString = "You are pretty antisocial."}else
 			if(chaTemp < 7){chaString = "Your charisma is average."}else
 			if(chaTemp < 9){chaString = "You are pretty social."}else
-			{chaString = "You are a social butterfly. Everyone wants to be your best pal!"};
-			
+			{chaString = "You are a social butterfly. Everyone wants to be your best pal!"}
+
 			var skillStrings = [];
 			var tempSkills = form.getElementsByClassName('skillz');
 			var checkedPoint = 0;
 			var checkedSkills = [];
-			for(var i = 0; i < tempSkills.length; i++){
+			for(i = 0; i < tempSkills.length; i++){
 				if(tempSkills[i].checked === true){
 					checkedSkills[checkedPoint] = tempSkills[i].name;
 					checkedPoint++;
 				}
 			}
-			
-			for(var i = 0; i < 3; i++){
+
+			for(i = 0; i < 3; i++){
 				switch(checkedSkills[i]){
 					case "blades":
 						skillStrings[i] = "formidable with a sword";
@@ -283,19 +284,19 @@ var Generator = (function() {
 						break;
 					case "math":
 						skillStrings[i] = "a brilliant mathematician";
-						
+
 				}
 			}
-			
-			
-			
+
+
+
 			charString = "You are " + nameString + ": a " + ageString + " years old " + raceString + " " + genderString + ". Today you are about to embark on an ADVENTURE! \n\n" + strString + "\n\n" + agiString + "\n\n" + conString + "\n\n" + intString + "\n\n" + chaString + "\n\nYour skills will aid you well in your journey: You are " + skillStrings[0] + ", " + skillStrings[1] + " and " + skillStrings[2] + ".\n\nYou gather what little you can call your own and set out into the unknown. What will await you? Fame, riches, glory? Or will you find nothing but hardship, despair and death? Only time will tell. All you know is that fortune favours the bold!"
 		}
-		
-		
+
+
 		description.value = charString;
 	}
-	
+
 	return {
 		init: init
 	};
